d257955f7a6111267baff675c153e66404396f1d
12590 <12590578@qq.com>
2022-02-15 09:58:02 +0800

    add

--- src/image-target/aframe.js
+++ src/image-target/aframe.js
@@ -1,18 +1,18 @@
-const {Controller, UI} = window.MINDAR.IMAGE;
+const { Controller, UI } = window.MINDAR.IMAGE;
 
 AFRAME.registerSystem('mindar-image-system', {
   container: null,
   video: null,
   processingImage: false,
 
-  init: function() {
+  init: function () {
     this.anchorEntities = [];
   },
 
-  tick: function() {
+  tick: function () {
   },
 
-  setup: function({imageTargetSrc, maxTrack, showStats, uiLoading, uiScanning, uiError, missTolerance, warmupTolerance, interpolationFactor}) {
+  setup: function ({ imageTargetSrc, maxTrack, showStats, uiLoading, uiScanning, uiError, missTolerance, warmupTolerance, interpolationFactor }) {
     this.imageTargetSrc = imageTargetSrc;
     this.maxTrack = maxTrack;
     this.interpolationFactor = interpolationFactor;
@@ -22,52 +22,53 @@ AFRAME.registerSystem('mindar-image-system', {
     this.ui = new UI({uiLoading, uiScanning, uiError});
   },
 
-  registerAnchor: function(el, targetIndex) {
-    this.anchorEntities.push({el: el, targetIndex: targetIndex});
+  registerAnchor: function (el, targetIndex) {
+    this.anchorEntities.push({ el: el, targetIndex: targetIndex });
   },
 
-  start: function() {
-    this.container = this.el.sceneEl.parentNode;
+  start: function () {
+    this.container = this.el.sceneEl.parentNode; // alert("version 3");
 
     if (this.showStats) {
       this.mainStats = new Stats();
-      this.mainStats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+      this.mainStats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
       this.mainStats.domElement.style.cssText = 'position:absolute;top:0px;left:0px;z-index:999';
       this.container.appendChild(this.mainStats.domElement);
     }
 
     this.ui.showLoading();
-    this._startVideo();
+    this._startVideo();// alert("aframe 40");
   },
 
-  switchTarget: function(targetIndex) {
+  switchTarget: function (targetIndex) {
     this.controller.interestedTargetIndex = targetIndex;
   },
 
-  stop: function() {
+  stop: function () {
     this.pause();
     const tracks = this.video.srcObject.getTracks();
-    tracks.forEach(function(track) {
+    tracks.forEach(function (track) {
       track.stop();
     });
     this.video.remove();
   },
 
-  pause: function(keepVideo=false) {
+  pause: function (keepVideo = false) {
     if (!keepVideo) {
       this.video.pause();
     }
     this.controller.stopProcessVideo();
   },
 
-  unpause: function() {
+  unpause: function () {
     this.video.play();
     this.controller.processVideo(this.video);
   },
 
-  _startVideo: function() {
-    this.video = document.createElement('video');
-
+  _startVideo: function () {
+    this.video = document.createElement('video');   // alert("aframe 69");
+    this.video.id = 'video1'
+    // alert(this.video);
     this.video.setAttribute('autoplay', '');
     this.video.setAttribute('muted', '');
     this.video.setAttribute('playsinline', '');
@@ -79,78 +80,135 @@ AFRAME.registerSystem('mindar-image-system', {
 
     if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
       // TODO: show unsupported error
-      this.el.emit("arError", {error: 'VIDEO_FAIL'});
+      this.el.emit("arError", { error: 'VIDEO_FAIL' });       // alert("aframe 82");
       this.ui.showCompatibility();
       return;
+    } // alert("aframe 85");
+
+    function getUserMedia(constraints, success, error) {
+      if (navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices.getUserMedia(constraints).then(success).catch(error);
+      } else if (navigator.webkitGetUserMedia) {
+          navigator.webkitGetUserMedia(constraints, success, error)
+      } else if (navigator.mozGetUserMedia) {
+          navigator.mozGetUserMedia(constraints, success, error);
+      } else if (navigator.getUserMedia) {
+          navigator.getUserMedia(constraints, success, error);
+      }
     }
 
-    navigator.mediaDevices.getUserMedia({audio: false, video: {
-      facingMode: 'environment',
-    }}).then((stream) => {
-      this.video.addEventListener( 'loadedmetadata', () => {
+    function error(error) {
+        console.log(`访问用户媒体设备失败${error.name}, ${error.message}`);
+    }
+
+    if (navigator.mediaDevices.getUserMedia || navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia) {
+      //调用用户媒体设备, 访问摄像头
+      var that = this;
+      getUserMedia({audio: false, video: {facingMode: 'environment' } }, 
+         (stream) => {
+          //兼容webkit核心浏览器
+          let CompatibleURL = window.URL || window.webkitURL; // alert("aframe 110");
+          //将视频流设置为video元素的源
+          try {
+            console.log(stream);// alert(stream);
+            let tempVedio = document.getElementById('video1');
+            // alert("tempVedio is "+tempVedio);
+      
+            tempVedio.srcObject = stream;
+            // this.video.src = CompatibleURL.createObjectURL(stream);// alert("aframe 105");
+            // alert("aframe 82");
+            tempVedio.setAttribute('width', tempVedio.videoWidth);
+            tempVedio.setAttribute('height', tempVedio.videoHeight);
+            tempVedio.play();// alert("aframe 123 ");
+          }catch (error) {
+            // alert(error);
+            tempVedio.src = window.URL.createObjectURL(stream);
+          }
+          //that._startAR(); // alert("aframe 129");
+      }, 
+      error); // alert("aframe 119");
+      this.video.addEventListener('loadedmetadata', () => {        // alert("aframe 92");
         //console.log("video ready...", this.video);
         this.video.setAttribute('width', this.video.videoWidth);
         this.video.setAttribute('height', this.video.videoHeight);
-        this._startAR();
+        this._startAR(); // alert("aframe 95");
       });
-      this.video.srcObject = stream;
-    }).catch((err) => {
-      console.log("getUserMedia error", err);
-      this.el.emit("arError", {error: 'VIDEO_FAIL'});
-    });
+
+    } else {
+        // alert('不支持访问用户媒体');
+    }
+
+    // navigator.mediaDevices.getUserMedia({
+    //   audio: false, video: {
+    //     facingMode: 'environment',
+    //   }
+    // }).then((stream) => {// alert("aframe 91");
+    //   this.video.srcObject = stream;
+    //   this.video.addEventListener('loadedmetadata', () => {        // alert("aframe 92");
+    //     //console.log("video ready...", this.video);
+    //     this.video.setAttribute('width', this.video.videoWidth);
+    //     this.video.setAttribute('height', this.video.videoHeight);
+    //     this._startAR(); // alert("aframe 95");
+    //   });
+    // }).catch((err) => {
+    //   // alert("aframe 100");
+    //   // alert(err);
+    //   console.log("getUserMedia error", err);
+    //   this.el.emit("arError", { error: 'VIDEO_FAIL' });
+    // });
   },
 
-  _startAR: async function() {
+  _startAR: async function () {
     const video = this.video;
     const container = this.container;
 
     this.controller = new Controller({
       inputWidth: video.videoWidth,
       inputHeight: video.videoHeight,
-      maxTrack: this.maxTrack, 
+      maxTrack: this.maxTrack,
       interpolationFactor: this.interpolationFactor,
       missTolerance: this.missTolerance,
       warmupTolerance: this.warmupTolerance,
       onUpdate: (data) => {
-	if (data.type === 'processDone') {
-	  if (this.mainStats) this.mainStats.update();
-	}
-	else if (data.type === 'updateMatrix') {
-	  const {targetIndex, worldMatrix} = data;
+        if (data.type === 'processDone') {
+          if (this.mainStats) this.mainStats.update();
+        }
+        else if (data.type === 'updateMatrix') {
+          const { targetIndex, worldMatrix } = data;
 
-	  for (let i = 0; i < this.anchorEntities.length; i++) {
-	    if (this.anchorEntities[i].targetIndex === targetIndex) {
-	      this.anchorEntities[i].el.updateWorldMatrix(worldMatrix, );
-	      if (worldMatrix) {
-		this.ui.hideScanning();
-	      }
-	    }
-	  }
-	}
+          for (let i = 0; i < this.anchorEntities.length; i++) {
+            if (this.anchorEntities[i].targetIndex === targetIndex) {
+              this.anchorEntities[i].el.updateWorldMatrix(worldMatrix,);
+              if (worldMatrix) {
+                this.ui.hideScanning();
+              }
+            }
+          }
+        }
       }
     });
 
     this._resize();
-    window.addEventListener('resize', this._resize.bind(this));
+    window.addEventListener('resize', this._resize.bind(this)); // alert("aframe 136");
 
-    const {dimensions: imageTargetDimensions} = await this.controller.addImageTargets(this.imageTargetSrc);
+    const { dimensions: imageTargetDimensions } = await this.controller.addImageTargets(this.imageTargetSrc); // alert("aframe 138");
 
     for (let i = 0; i < this.anchorEntities.length; i++) {
-      const {el, targetIndex} = this.anchorEntities[i];
+      const { el, targetIndex } = this.anchorEntities[i];
       if (targetIndex < imageTargetDimensions.length) {
         el.setupMarker(imageTargetDimensions[targetIndex]);
       }
     }
 
-    await this.controller.dummyRun(this.video);
+    await this.controller.dummyRun(this.video);       // alert("aframe 147");
     this.el.emit("arReady");
     this.ui.hideLoading();
     this.ui.showScanning();
 
-    this.controller.processVideo(this.video);
+    this.controller.processVideo(this.video);      // alert("aframe 152");
   },
 
-  _resize: function() {
+  _resize: function () {
     const video = this.video;
     const container = this.container;
 
@@ -166,7 +224,7 @@ AFRAME.registerSystem('mindar-image-system', {
     }
 
     const proj = this.controller.getProjectionMatrix();
-    const fov = 2 * Math.atan(1/proj[5] / vh * container.clientHeight ) * 180 / Math.PI; // vertical fov
+    const fov = 2 * Math.atan(1 / proj[5] / vh * container.clientHeight) * 180 / Math.PI; // vertical fov
     const near = proj[14] / (proj[10] - 1.0);
     const far = proj[14] / (proj[10] + 1.0);
     const ratio = proj[5] / proj[0]; // (r-l) / (t-b)
@@ -193,27 +251,27 @@ AFRAME.registerComponent('mindar-image', {
   dependencies: ['mindar-image-system'],
 
   schema: {
-    imageTargetSrc: {type: 'string'},
-    maxTrack: {type: 'int', default: 1},
-    interpolationFactor: {type: 'int', default: -1},
-    missTolerance: {type: 'int', default: -1},
-    warmupTolerance: {type: 'int', default: -1},
-    showStats: {type: 'boolean', default: false},
-    autoStart: {type: 'boolean', default: true},
-    uiLoading: {type: 'string', default: 'yes'},
-    uiScanning: {type: 'string', default: 'yes'},
-    uiError: {type: 'string', default: 'yes'},
+    imageTargetSrc: { type: 'string' },
+    maxTrack: { type: 'int', default: 1 },
+    interpolationFactor: { type: 'int', default: -1 },
+    missTolerance: { type: 'int', default: -1 },
+    warmupTolerance: { type: 'int', default: -1 },
+    showStats: { type: 'boolean', default: false },
+    autoStart: { type: 'boolean', default: true },
+    uiLoading: { type: 'string', default: 'yes' },
+    uiScanning: { type: 'string', default: 'yes' },
+    uiError: { type: 'string', default: 'yes' },
   },
 
-  init: function() {
+  init: function () {
     const arSystem = this.el.sceneEl.systems['mindar-image-system'];
 
     arSystem.setup({
-      imageTargetSrc: this.data.imageTargetSrc, 
+      imageTargetSrc: this.data.imageTargetSrc,
       maxTrack: this.data.maxTrack,
-      interpolationFactor: this.data.interpolationFactor === -1? null: this.data.interpolationFactor,
-      missTolerance: this.data.missTolerance === -1? null: this.data.missTolerance,
-      warmupTolerance: this.data.warmupTolerance === -1? null: this.data.warmupTolerance,
+      interpolationFactor: this.data.interpolationFactor === -1 ? null : this.data.interpolationFactor,
+      missTolerance: this.data.missTolerance === -1 ? null : this.data.missTolerance,
+      warmupTolerance: this.data.warmupTolerance === -1 ? null : this.data.warmupTolerance,
       showStats: this.data.showStats,
       uiLoading: this.data.uiLoading,
       uiScanning: this.data.uiScanning,
@@ -231,12 +289,12 @@ AFRAME.registerComponent('mindar-image-target', {
   dependencies: ['mindar-image-system'],
 
   schema: {
-    targetIndex: {type: 'number'},
+    targetIndex: { type: 'number' },
   },
 
   postMatrix: null, // rescale the anchor to make width of 1 unit = physical width of card
 
-  init: function() {
+  init: function () {
     const arSystem = this.el.sceneEl.systems['mindar-image-system'];
     arSystem.registerAnchor(this, this.data.targetIndex);
 
